66d8597ca75fdf75ffe5ee8626696a9022ead181
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-18 17:26:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import datetime
 
 with st.form("Introduzca sus datos"):
   nombre = st.text_input("Nombre del alumno")  
-  fecha_nac = st.date_input()
+  fecha_nac = st.date_input("Fecha de nacimiento")
   alergias = st.multiselect("Alergias",["Frutos secos", "Lácteos", "Gluten", "Otros"])
   # El botón Enviar es obligatorio
   enviado = st.form_submit_button("Enviar")
