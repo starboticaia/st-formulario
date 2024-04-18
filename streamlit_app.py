1c296a9ff495241d9144720a2210fac90d15c861
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-18 17:37:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,25 @@
 import streamlit as st
-import datetime
 
 with st.form("Introduzca sus datos"):
-  nombre = st.text_input("Nombre del alumno")  
+  nombre = st.text_input("Nombre")
   fecha_nac = st.date_input("Fecha de nacimiento")
   alergias = st.multiselect("Alergias",["Frutos secos", "Lácteos", "Gluten", "Otros"])
   # El botón Enviar es obligatorio
   enviado = st.form_submit_button("Enviar")
   
   if enviado:
-    hoy = datetime.date.today()
-    # la diferencia está en días
-    edad = (hoy - fecha_nac)/365
+    hoy = datetime.date.today()      
     st.write("Nombre",nombre)
-    st.write("Edad",edad)
+    if alergias.length>0:
+      st.write("Asegúrese de que ha especificado todas sus alergias.")            
+    else:
+      no_alergico = st.checkbox("Marque la casilla para confirmar que declara que no es alérgico")
+      alergias = "Ninguna"
+
+    resumen = f"""
+    Nombre: {nombre}
+    Edad: {edad}
+    Alergias: {alergias}
+    """
+    
+    st.download()
