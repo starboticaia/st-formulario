5dbf68464eb7e2f8251f8234295ba6a2ed65a6c2
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-18 17:16:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from datetime import date
 
-st.write("Hola mundo")
+with st.form("Introduzca sus datos"):
+  nombre = st.text_input("Nombre del alumno")  
+  fecha_nac = st.date()
+  alergias = st.multiselect("Frutos secos", "Lácteos", "Gluten", "Otros")
+  # El botón Enviar es obligatorio
+  enviado = st.form_submit_button("Enviar")
+  
+  if enviado:
+    hoy = date.today()
+    edad_dias = hoy - fecha_nac
+    st.write("Nombre",nombre)
+    st.write("Edad",edad_dias)
